c0c8e04aeaf62455b6e89bfa0ffe98d61f4d60bb
jjang-youngju <zxc74100@naver.com>
2022-12-13 17:26:07 +0900

    1213

---
+++ 오후수업/spring/member/src/main/java/com/dw/member/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.dw.member.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dw.member.model.Member;
+import com.dw.member.repository.MemberRepo;
+
+@RestController
+public class MemberController {
+
+	@Autowired
+	MemberRepo repo;
+	
+	// 전체조회
+	@GetMapping("/members")
+	public List<Member> callAllMembers(){
+		//findAll == select * from <테이블이름>
+		return repo.findAll();
+	}
+	
+	//추가
+	@PostMapping("/member")
+	public Member callSaveMember(@RequestBody Member member) {
+		//save == insert
+		member = repo.save(member);
+		return member;
+	}
+	
+	
+}
